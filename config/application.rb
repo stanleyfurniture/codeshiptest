d33b66da3b14c80560ab2b352d13e2cbe3d9e4ba
Rob Zimmerman <taintedz@gmail.com>
2014-01-30 11:54:10 -0500

    Fix a crash when Heroku tries to precompile assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Codeshiptest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Prevent Heroku deploy from crashing on precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
