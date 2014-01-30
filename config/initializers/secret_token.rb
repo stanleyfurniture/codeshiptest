095e4f42ef3c72d97d58268b059781af688d4d64
Mike Clark <mclark@stanleyfurniture.com>
2014-01-30 09:42:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codeshiptest::Application.config.secret_token = '3c3ed129c33217962446eec559cdac5dfee201f92f8b1df55d9dc9a3510f9c7f9c024f7a02b7aded8ff33e07856487bd5e8e680e9ec1272b980e82b85b94547c'
